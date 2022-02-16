613e14794872870eb27cb62ce39470c10329c4d6
Dark Knight <souviksen827@gmail.com>
2022-02-16 13:43:11 +0530

    Implemented subtract() fn

--- math.py
+++ math.py
@@ -4,7 +4,7 @@ def add(x,y):
 	
 #Subtract Implementation 2
 def subtract(x,y):
-	pass              
+	return x-y          #on Bug456 branch             
 	
 #Multiply Implementation 2
 def multiply(x,y):
